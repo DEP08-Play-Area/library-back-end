54b772584fa39f1df616a6fc3e34c55bad0f0df6
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2022-04-25 15:36:34 +0530

    Add MemberDTO.java

---
+++ src/main/java/lk/ijse/dep8/library/dto/MemberDTO.java
@@ -0,0 +1,40 @@
+package lk.ijse.dep8.library.dto;
+
+public class MemberDTO {
+    private String nic;
+    private String name;
+    private String contact;
+
+    public MemberDTO(String nic, String name, String contact) {
+        this.nic = nic;
+        this.name = name;
+        this.contact = contact;
+    }
+
+    public MemberDTO() {
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+}
