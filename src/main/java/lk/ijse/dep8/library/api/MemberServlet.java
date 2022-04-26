e293f576f5d654775d87893dc80c38b8fc9b8b7a
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2022-04-26 07:53:41 +0530

    Finalize Search Member

--- src/main/java/lk/ijse/dep8/library/api/MemberServlet.java
+++ src/main/java/lk/ijse/dep8/library/api/MemberServlet.java
@@ -17,6 +17,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 @WebServlet(name = "MemberServlet", value = {"/members/*"})
@@ -25,93 +27,87 @@ public class MemberServlet extends HttpServlet {
     @Resource (name = "java:comp/env/jdbc/pool4library")
     private volatile DataSource pool;
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getPathInfo() != null && !req.getPathInfo().equals("/")){
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        String query = req.getParameter("q");
+        query = "%"  + ((query == null) ? "" : query) + "%";
+
+        try (Connection connection = pool.getConnection()) {
+            boolean pagination = req.getParameter("page") != null &&
+                    req.getParameter("size") != null;
+            String sql = null;
+
+            if (pagination) {
+                sql = "SELECT * FROM member WHERE nic LIKE ? OR name LIKE ? OR contact LIKE ? LIMIT ? OFFSET ?";
+            } else {
+                sql = "SELECT * FROM member WHERE nic LIKE ? OR name LIKE ? OR contact LIKE ?";
+            }
+
+            PreparedStatement stm = connection.prepareStatement(sql);
+            PreparedStatement stmCount = connection.prepareStatement("SELECT count(*) FROM member WHERE nic LIKE ? OR name LIKE ? OR contact LIKE ?");
+            stm.setString(1, query);
+            stm.setString(2, query);
+            stm.setString(3, query);
+            stmCount.setString(1, query);
+            stmCount.setString(2, query);
+            stmCount.setString(3, query);
+
+            if (pagination){
+                int page = Integer.parseInt(req.getParameter("page"));
+                int size = Integer.parseInt(req.getParameter("size"));
+                stm.setInt(4, size);
+                stm.setInt(5, (page - 1) * size);
+            }
+            ResultSet rst = stm.executeQuery();
+
+            List<MemberDTO> members = new ArrayList<>();
+
+            while (rst.next()){
+                members.add((new MemberDTO(
+                        rst.getString("nic"),
+                        rst.getString("name"),
+                        rst.getString("contact")
+                )));
+            }
+
+            resp.setContentType("application/json");
+            resp.setHeader("X-Count", members.size() + "");
+
+            if (!pagination) {
+                resp.setHeader("X-Count", members.size() + "");
+            }else{
+                ResultSet rst2 = stmCount.executeQuery();
+                if (rst2.next()){
+                    resp.setHeader("X-Count", rst2.getString(1));
+                }
+            }
+
+            Jsonb jsonb = JsonbBuilder.create();
+            jsonb.toJson(members, resp.getWriter());
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getContentType() == null || !request.getContentType().toLowerCase().startsWith("application/json")){
-            response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
-            return;
-        }
-
-        try{
-            Jsonb jsonb = JsonbBuilder.create();
-            MemberDTO member = jsonb.fromJson(request.getReader(), MemberDTO.class);
-
-            if (member.getNic() == null || !member.getNic().matches("\\d{9}[Vv]")){
-                throw new ValidationException("Invalid NIC");
-            } else if(member.getName() == null || !member.getName().matches("[A-Za-z ]+")){
-                throw new ValidationException("Invalid Name");
-            } else if(member.getContact() == null || !member.getContact().matches("\\d{3}-\\d{7}")){
-                throw new ValidationException("Invalid Contact number");
-            }
-
-            //Try to register the member,according to the result, we need to send the response
-            try (Connection connection = pool.getConnection()) {
-                PreparedStatement stm = connection.prepareStatement("SELECT * FROM member WHERE nic=?");
-                stm.setString(1,member.getNic());
-                ResultSet rst = stm.executeQuery();
-                if (rst.next()){
-                    response.sendError(HttpServletResponse.SC_CONFLICT,"Member already exists");
-                    return;
-                }
-
-                stm = connection.prepareStatement("INSERT INTO member (nic, name, contact) VALUES (?,?,?)");
-                stm.setString(1, member.getNic());
-                stm.setString(2, member.getName());
-                stm.setString(3, member.getContact());
-                if (stm.executeUpdate() != 1){
-                    throw new RuntimeException("Failed to register the member");
-                }
-                response.setStatus(HttpServletResponse.SC_CREATED);
-            }
-        } catch (JsonbException | ValidationException e) {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST,
-                    (e instanceof JsonbException) ? "Invalid JSON" : e.getMessage());
-        } catch (Throwable t){
-            t.printStackTrace();
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        }
-
+        doSaveOrUpdate(request,response);
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getPathInfo() == null){
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND,"Member does not exist");
-            return;
-        } else if (req.getContentType().toLowerCase(Locale.ROOT).startsWith("application/json")){
-            resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
-            return;
-        }
-
-        try{
-            Jsonb jsonb = JsonbBuilder.create();
-            MemberDTO member = jsonb.fromJson(req.getReader(), MemberDTO.class);
-        }catch (JsonbException e){
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST,"Invalid Json");
-        }
-
-        try(Connection connection = pool.getConnection()){
-            PreparedStatement stm = connection.prepareStatement("SELECT * FROM member WHERE nic=?");
-            stm.setString(1,req.getPathInfo().substring(1));
-            ResultSet rst = stm.executeQuery();
-
-            if (!rst.next()){
-                doPost(req,resp);
-            } else {
-
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        doSaveOrUpdate(req,resp);
     }
 
     private void doSaveOrUpdate(HttpServletRequest req, HttpServletResponse res) throws IOException{
-        if ((req.getContentType() == null) || !req.getContentType().toLowerCase().startsWith("application/json")){
+        if (req.getContentType() == null ||
+                !req.getContentType().toLowerCase().startsWith("application/json")) {
             res.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
             return;
         }
@@ -121,11 +117,11 @@ public class MemberServlet extends HttpServlet {
 
         if (method.equals("POST") &&
                 !((req.getServletPath().equalsIgnoreCase("/members") ||
-                        req.getServletPath().equalsIgnoreCase("/members/")))){
+                        req.getServletPath().equalsIgnoreCase("/members/")))) {
             res.sendError(HttpServletResponse.SC_NOT_FOUND);
             return;
-        }else if (method.equals("PUT") && !(pathInfo != null &&
-                pathInfo.substring(1).matches("\\d{9}[Vv][/]?"))){
+        } else if (method.equals("PUT") && !(pathInfo != null &&
+                pathInfo.substring(1).matches("\\d{9}[Vv][/]?"))) {
             res.sendError(HttpServletResponse.SC_NOT_FOUND, "Member does not exist");
             return;
         }
@@ -142,29 +138,29 @@ public class MemberServlet extends HttpServlet {
                 throw new ValidationException("Invalid contact number");
             }
 
-            if (method.equals("PUT")){
+            if (method.equals("PUT")) {
                 member.setNic(pathInfo.replaceAll("[/]", ""));
             }
 
             try (Connection connection = pool.getConnection()) {
                 PreparedStatement stm = connection.prepareStatement("SELECT * FROM member WHERE nic=?");
-                stm.setString(1,  member.getNic());
+                stm.setString(1, member.getNic());
                 ResultSet rst = stm.executeQuery();
 
                 if (rst.next()) {
-                    if (method.equals("POST")){
+                    if (method.equals("POST")) {
                         res.sendError(HttpServletResponse.SC_CONFLICT, "Member already exists");
-                    }else{
-                        stm =  connection.prepareStatement("UPDATE member SET name=?, contact=? WHERE nic=?");
+                    } else {
+                        stm = connection.prepareStatement("UPDATE member SET name=?, contact=? WHERE nic=?");
                         stm.setString(1, member.getName());
                         stm.setString(2, member.getContact());
                         stm.setString(3, member.getNic());
-                        if (stm.executeUpdate() != 1){
+                        if (stm.executeUpdate() != 1) {
                             throw new RuntimeException("Failed to update the member");
                         }
                         res.setStatus(HttpServletResponse.SC_NO_CONTENT);
                     }
-                }else{
+                } else {
                     stm = connection.prepareStatement("INSERT INTO member (nic, name, contact) VALUES (?,?,?)");
                     stm.setString(1, member.getNic());
                     stm.setString(2, member.getName());
@@ -179,11 +175,12 @@ public class MemberServlet extends HttpServlet {
         } catch (JsonbException | ValidationException e) {
             res.sendError(HttpServletResponse.SC_BAD_REQUEST,
                     (e instanceof JsonbException) ? "Invalid JSON" : e.getMessage());
-        } catch (Throwable t){
+        } catch (Throwable t) {
             t.printStackTrace();
             res.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
+
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
